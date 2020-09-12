1e877a0cfc664568334ebc5356ab0868aba21c49
Dy0mar <kurtudins@gmail.com>
2020-09-12 14:21:33 +0300

    fix create

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 import os
 
-from app import app, manager
+from app import app, db
 
 if __name__ == "__main__":
-    manager.run()
     app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 8080))
+    db.drop_all()
+    db.create_all()
